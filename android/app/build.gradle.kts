893caac8a70e0c6ea1bb7b44f9a9c5f2e9e73ca6
Max Art <maxartk@gmail.com>
2026-04-07 00:11:54 +0200

    Fix: explicit minSdk 26, compileSdk 35, ndkVersion 27.0.12077973

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 
 android {
     namespace = "com.maxart.snore_cost"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -20,14 +20,11 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.maxart.snore_cost"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
     }
 
     buildTypes {
