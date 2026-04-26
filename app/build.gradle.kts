ff352dc7b71f6d098fa05423a478ff292c2508dc
Max Kovalevsky <max.kovalevsky@gmail.com>
2026-04-26 07:29:32 +0200

    fix: Gradle config - dependencyResolutionManagement, Compose compiler plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -28,14 +29,6 @@ android {
 
     kotlinOptions {
         jvmTarget = "17"
-    }
-
-    buildFeatures {
-        compose = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
     }
 }
 
